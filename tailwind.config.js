e39e87efeef4a2dc98cfc4280cd2cf8d959a4ca3
thong thai <125822457+thongthapcode@users.noreply.github.com>
2025-04-02 16:39:29 +0700

    Add files via upload
    Signed-off-by: thong thai  <125822457+thongthapcode@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}" // Đảm bảo đường dẫn đúng
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
